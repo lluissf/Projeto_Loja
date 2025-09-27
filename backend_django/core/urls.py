d6ba10a209503c8814558dd05dcc24fbc780e73d
Luís Felipe <luis.felipe.ferreira.da.silva@hotmail.com>
2025-09-27 10:55:57 -0300

    /api/ funciona

--- backend_django/core/urls.py
+++ backend_django/core/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from produtos.views import home
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from produtos.views import ProdutoViewSet
 
+# Criando o router
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet)
+
+# Definindo as rotas
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('api/', include(router.urls)),
 ]
